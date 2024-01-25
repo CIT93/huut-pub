1db2b8c7916c04fa2e4b591880036f4c91442b07
Thuh <thuh@netcompany.com>
2024-01-25 23:47:53 +0700

    functions

--- w3/main.js
+++ w3/main.js
@@ -1,22 +1,31 @@
-let cardonFootprintPoints = 0;
-const numberInHousehold = 5;
+function determineHouseholdPts(numberInHousehold) {
+   console.log("Inside the function");
 
-if (numberInHousehold === 1){
-    cardonFootprintPoints = cardonFootprintPoints + 14;
-}  else if (numberInHousehold === 2){
-    cardonFootprintPoints = cardonFootprintPoints + 12; 
-}  else if (numberInHousehold === 3){
-    cardonFootprintPoints = cardonFootprintPoints + 10;
-}  else if (numberInHousehold === 4){
-    cardonFootprintPoints = cardonFootprintPoints + 8;
-}  else if (numberInHousehold === 5){
-    cardonFootprintPoints = cardonFootprintPoints + 6;
-}  else if (numberInHousehold === 6){
-    cardonFootprintPoints = cardonFootprintPoints + 4;
-}  else if (numberInHousehold > 6){
-    cardonFootprintPoints = cardonFootprintPoints + 2;
-} else {
-    console.log("not points updated");
+   if (numberInHousehold === 1){
+      cardonFootprintPoints = cardonFootprintPoints + 14;
+  }  else if (numberInHousehold === 2){
+      cardonFootprintPoints = cardonFootprintPoints + 12; 
+  }  else if (numberInHousehold === 3){
+      cardonFootprintPoints = cardonFootprintPoints + 10;
+  }  else if (numberInHousehold === 4){
+      cardonFootprintPoints = cardonFootprintPoints + 8;
+  }  else if (numberInHousehold === 5){
+      cardonFootprintPoints = cardonFootprintPoints + 6;
+  }  else if (numberInHousehold === 6){
+      cardonFootprintPoints = cardonFootprintPoints + 4;
+  }  else if (numberInHousehold > 6){
+      cardonFootprintPoints = cardonFootprintPoints + 2;
+  } else {
+      console.log("not points updated");
+  }
+  
+  console.log(`Based on the number of member of the household of ${numberInHousehold} the points would be ${cardonFootprintPoints}`);
+  
 }
 
-console.log(`Based on the number of member of the household of ${numberInHousehold} the points would be ${cardonFootprintPoints}`);
+let cardonFootprintPoints = 0;
+// const numberInHousehold = 5;
+
+determineHouseholdPts(4);
+determineHouseholdPts(5);
+
