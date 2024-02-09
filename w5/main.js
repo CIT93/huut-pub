392ba4934a94db4bb861ccd115e91d99d3ac6752
Thuh <thuh@netcompany.com>
2024-02-09 22:25:49 +0700

    setup and array of objects

---
+++ w5/main.js
@@ -0,0 +1,96 @@
+const cfpData = [];
+
+function determineHouseholdPts(numberInHousehold) {
+   let houseHoldPoints = 0;
+   if (numberInHousehold === 1){
+    houseHoldPoints = 14;
+  }  else if (numberInHousehold === 2){
+    houseHoldPoints = 12; 
+  }  else if (numberInHousehold === 3){
+    houseHoldPoints = 10;
+  }  else if (numberInHousehold === 4){
+    houseHoldPoints = 8;
+  }  else if (numberInHousehold === 5){
+    houseHoldPoints = 6;
+  }  else if (numberInHousehold === 6){
+    houseHoldPoints = 4;
+  }  else if (numberInHousehold > 6){
+    houseHoldPoints = 2;
+  } else {
+      console.log("not points updated");
+  }
+  
+  return houseHoldPoints;
+}
+
+function determineHouseholdSizePts(homeType) {
+  let houseHoldSizePoints = 0;
+
+   switch(homeType) {
+       case 'large':
+        houseHoldSizePoints += 10;
+         break;
+       case 'medium':
+        houseHoldSizePoints += 7;
+         break;
+       case 'small':
+        houseHoldSizePoints += 4;
+         break;
+       case 'apartment':
+        houseHoldSizePoints += 2;
+         break;
+       default:
+         console.log('Invalid home type');
+   }
+
+   return houseHoldSizePoints;
+}
+
+function start(houseHoldMembers, houseSize){
+  const houseHoldPts = determineHouseholdPts(houseHoldMembers);
+  console.log(houseHoldPts);
+
+  const houseHoldSizePts = determineHouseholdSizePts(houseSize);
+  console.log(houseHoldSizePts);
+
+  const totalPts = houseHoldPts + houseHoldSizePts;
+
+  const cfpObject = {
+    memberInHouseHold : houseHoldMembers,
+    sizeHouse: houseSize,
+    househouseSPts : houseHoldSizePts,
+    householdMPts: houseHoldPts,
+    cfpTotal: totalPts
+  }
+
+  cfpData.push(cfpObject);
+
+  // displayOutput(cfpObject);
+  // cfpData.push([houseHoldMembers, houseSize, houseHoldPts, houseHoldSizePts, totalPts]);
+}
+
+function displayOutput(){
+  for (obj of cfpData){
+    console.log(obj);
+    const output = document.getElementById("output");
+    const newP = document.createElement("p");
+    newP.textContent = `Cardon Footprint total is ${obj.cfpTotal}`;
+    
+    // const newHouseHoldLi = document.createElement("li");
+    // newHouseHoldLi.textContent = `Number people in the house is ${arr[0]} with score ${arr[2]}`;
+
+    // const newHouseHoldSizeLi = document.createElement("li");
+    // newHouseHoldSizeLi.textContent =  `${arr[1]} size of home with score ${arr[3]}`;
+
+    output.appendChild(newP);
+    // output.appendChild(newHouseHoldLi);
+    // output.appendChild(newHouseHoldSizeLi);
+  }
+}
+
+start(2, "small");
+start(3, "medium");
+start(4, "large");
+start(5, "apartment");
+
+displayOutput();
