410966faa13eb26da05d3d02aa9f678143ea233c
Thuh <thuh@netcompany.com>
2024-02-23 11:13:46 +0700

    challenge 1

--- w7/main.js
+++ w7/main.js
@@ -1,54 +1,9 @@
 import { renderTable } from "./render.js";
+import {determineHouseholdPts, determineHouseholdSizePts} from "./carbonFootprint.js"
 
 const FORM = document.getElementById("form");
 
 const cfpData = [];
-
-function determineHouseholdPts(numberInHousehold) {
-   let houseHoldPoints = 0;
-   if (numberInHousehold === 1){
-    houseHoldPoints = 14;
-  }  else if (numberInHousehold === 2){
-    houseHoldPoints = 12; 
-  }  else if (numberInHousehold === 3){
-    houseHoldPoints = 10;
-  }  else if (numberInHousehold === 4){
-    houseHoldPoints = 8;
-  }  else if (numberInHousehold === 5){
-    houseHoldPoints = 6;
-  }  else if (numberInHousehold === 6){
-    houseHoldPoints = 4;
-  }  else if (numberInHousehold > 6){
-    houseHoldPoints = 2;
-  } else {
-      console.log("not points updated");
-  }
-  
-  return houseHoldPoints;
-}
-
-function determineHouseholdSizePts(homeType) {
-  let houseHoldSizePoints = 0;
-
-   switch(homeType) {
-       case 'large':
-        houseHoldSizePoints += 10;
-         break;
-       case 'medium':
-        houseHoldSizePoints += 7;
-         break;
-       case 'small':
-        houseHoldSizePoints += 4;
-         break;
-       case 'apartment':
-        houseHoldSizePoints += 2;
-         break;
-       default:
-         console.log('Invalid home type');
-   }
-
-   return houseHoldSizePoints;
-}
 
 function start(firstname, lastname, houseHoldMembers, houseSize){
   const houseHoldPts = determineHouseholdPts(houseHoldMembers);
@@ -71,7 +26,6 @@ function start(firstname, lastname, houseHoldMembers, houseSize){
 
   cfpData.push(cfpObject);
 }
-
 
 addEventListener('submit', function(e){
   e.preventDefault();
